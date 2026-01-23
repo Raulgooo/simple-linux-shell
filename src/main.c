be6953bf54842dc1a81048363d804e00985b1df5
raulgpt <raulgooo@outlook.com>
2026-01-23 12:30:37 -0600

    codecrafters submit [skip ci]

--- src/main.c
+++ src/main.c
@@ -7,11 +7,14 @@ int main(int argc, char *argv[]) {
   setbuf(stdout, NULL);
   char command[1024];
   while (1) {
-  
-  
   printf("$ ");
   fgets(command, sizeof(command), stdin);
   command[strcspn(command, "\n")] = '\0';
+
+  if (strcmp(command, "exit") == 0) {
+    return 0;
+  }
+  
   printf("%s: command not found\n", command);
   }
 
