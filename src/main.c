4334e7e80e42e3361c736c0107ad80e526031c8d
raulgpt <raulgooo@outlook.com>
2026-01-29 12:20:58 -0600

    ok

--- src/main.c
+++ src/main.c
@@ -109,6 +109,9 @@ int main() {
         } 
         else if (strcmp(cmd, "cd") == 0) {
             char *path = (working_state.token_count > 1) ? working_state.tokens[1].text : getenv("HOME");
+            if (path == '~') {
+              path = getenv("HOME");
+            }
             if (chdir(path) != 0) perror("cd");
         }
         else {
